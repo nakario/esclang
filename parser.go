5e2c25df02945589a2bfbead222e7ba2242e0385
nakario <nakario.jp@gmail.com>
2019-05-31 14:10:37 +0900

    Fix a bug that label doesn't work correctly

--- parser.go
+++ parser.go
@@ -32,12 +32,14 @@ func (p *Parser) Parse() error {
 		case NormalToken:
 			Logger.Println("normal token")
 			if labeling {
+				Logger.Println("Create label:", string([]rune{token.Char()}))
 				_, ok := p.labels[token.Char()]
 				if ok {
 					return errors.New("duplicated label")
 				}
-				p.labels[token.Char()] = len(p.instructions) - 1
-			} else if bgInstruction != nil {
+				p.labels[token.Char()] = len(p.instructions)
+			}
+			if bgInstruction != nil {
 				p.instructions = append(p.instructions, bgInstruction(token.Char()))
 			}
 			if jumping {
@@ -47,48 +49,58 @@ func (p *Parser) Parse() error {
 				p.instructions = append(p.instructions, fgInstruction(token.Char()))
 			}
 		case _SGRToken:
-			Logger.Println("SGR token")
-			labeling = false
-			jumping = false
-			switch token.param {
-			case 30:
-				fgInstruction = copyInstruction
-			case 31:
-				fgInstruction = incrementInstruction
-			case 32:
-				fgInstruction = inputInstruction
-			case 33:
-				fgInstruction = rotateLeftInstruction
-			case 34:
-				fgInstruction = rotateRightInstruction
-			case 35:
-				fgInstruction = outputInstructioin
-			case 36:
-				fgInstruction = decrementInstruction
-			case 37:
-				fgInstruction = swapInstruction
-			case 39:
-				fgInstruction = nopInstruction
-			case 40:
-				bgInstruction = callInstruction
-			case 41:
-				bgInstruction = incrementPtrInstruction
-			case 42:
-				jumping = true
-				bgInstruction = jumpIfZeroInstruction
-			case 43:
-				bgInstruction = incrementPrimalPtrInstruction
-			case 44:
-				bgInstruction = decrementPrimalPtrInstruction
-			case 45:
-				labeling = true
-			case 46:
-				bgInstruction = decrementPtrInstruction
-			case 47:
-				bgInstruction = exitInstruction
-			case 49:
-				bgInstruction = nopInstruction
-			default:
+			Logger.Println("SGR token:", token.param)
+			if 30 <= token.param && token.param <= 39 {
+				switch token.param {
+				case 30:
+					fgInstruction = copyInstruction
+				case 31:
+					fgInstruction = incrementInstruction
+				case 32:
+					fgInstruction = inputInstruction
+				case 33:
+					fgInstruction = rotateLeftInstruction
+				case 34:
+					fgInstruction = rotateRightInstruction
+				case 35:
+					fgInstruction = outputInstructioin
+				case 36:
+					fgInstruction = decrementInstruction
+				case 37:
+					fgInstruction = swapInstruction
+				case 39:
+					fgInstruction = nopInstruction
+				default:
+					return errors.New("unexpected SGR parameter")
+				}
+			} else if 40 <= token.param && token.param <= 49 {
+				labeling = false
+				jumping = false
+				switch token.param {
+				case 40:
+					bgInstruction = callInstruction
+				case 41:
+					bgInstruction = incrementPtrInstruction
+				case 42:
+					jumping = true
+					bgInstruction = jumpIfZeroInstruction
+				case 43:
+					bgInstruction = incrementPrimalPtrInstruction
+				case 44:
+					bgInstruction = decrementPrimalPtrInstruction
+				case 45:
+					labeling = true
+					bgInstruction = nopInstruction
+				case 46:
+					bgInstruction = decrementPtrInstruction
+				case 47:
+					bgInstruction = exitInstruction
+				case 49:
+					bgInstruction = nopInstruction
+				default:
+					return errors.New("unexpected SGR parameter")
+				}
+			} else {
 				return errors.New("unexpected SGR parameter")
 			}
 		default:
